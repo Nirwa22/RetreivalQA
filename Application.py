7b7bf3409298a71afd54fef0fc05a7abdb0e6075
Nirwa22 <nyusra.msai23seecs@seecs.edu.pk>
2024-10-22 11:07:22 +0500

    new changes

--- Application.py
+++ Application.py
@@ -1,9 +1,15 @@
-from Database import vectordb
-from flask import Flask
+from Database import v_database
+from flask import Flask, request
 from flask_cors import CORS
+from langchain_community.vectorstores import FAISS
+from langchain_huggingface import HuggingFaceEmbeddings
 Application = Flask(__name__)
 CORS(Application)
 
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+new_vector_store = FAISS.load_local(
+    "Vectordb", embeddings, allow_dangerous_deserialization=True
+)
 
 @Application.route("/")
 def homepage():
@@ -13,14 +19,14 @@ def homepage():
 @Application.route("/Upload_files", methods=['POST'])
 def upload_files():
     input_file_path = "Pdf_files"
-    v_database = vectordb(input_file_path)
-    v_database.persist()
     return v_database
 
 
 @Application.route("/Enter_Query", methods=['POST'])
 def enter_query():
-    return "home"
+    input = request.get_json()
+    retriever = new_vector_store.as_retriever
+    return retriever.invoke(input["question"])
 
 
 if "__main__" == __name__:
